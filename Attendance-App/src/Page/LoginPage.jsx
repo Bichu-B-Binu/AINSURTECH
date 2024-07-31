48a7a3a69f65aff6a51a980bf04fdc343a77b9c0
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-31 17:33:56 +0530

    same

--- Attendance-App/src/Page/LoginPage.jsx
+++ Attendance-App/src/Page/LoginPage.jsx
@@ -24,7 +24,8 @@ export default function LoginPage() {
                   name="userId"
                   type="text"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Please enter your employee ID"
                 />
               </div>
             </div>
@@ -49,6 +50,7 @@ export default function LoginPage() {
                   type="password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Please enter your password"
                 />
               </div>
             </div>
